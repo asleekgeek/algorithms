2841f3d112b74245f6768b64616e15ba66d96dcb
Bojan Skrchevski <bojanskr@gmail.com>
2018-03-21 13:33:25 +0100

    Implementation of basic generic Binary Search Tree in C#

---
+++ data-structures/binarySearchTree/binary_search_tree.cs
@@ -0,0 +1,64 @@
+public class Node<T>
+{
+    public int key;
+    public T data;
+
+    public Node<T> left;
+    public Node<T> right;
+}
+
+public class BinarySearchTree<T>
+{
+    private Node<T> root = null;
+
+    public Node<T> Insert(int key, T value)
+    {
+        return InsertAtNode(root, key, value);
+    }
+
+    public void Traverse(Action<T> action)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        Traverse(root.left, action);
+        action(root.data);
+        Traverse(root.right, action);
+    }
+
+    private void Traverse(Node<T> node, Action<T> action)
+    {
+        if (root == null)
+        {
+            return;
+        }
+
+        Traverse(root.left, action);
+        action(root.data);
+        Traverse(root.right, action);
+    }
+
+    private Node<T> InsertAtNode(Node<T> node, int key, T value)
+    {
+        if (node == null) 
+        {
+            node = new Node<T>{ key = key, data = value };
+        }
+        else if (key == root.key) 
+        {
+            node.data = value;
+        }
+        else if (key < root.key) 
+        {
+            node.left = InsertAtNode(node.left, key, value);
+        }
+        else  // key > root.key
+        {
+            node.right = InsertAtNode(node.right, key, value);
+        }
+
+        return node;
+    }
+}
