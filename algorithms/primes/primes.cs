c598d598d6c7c95e57e77fe7f79a3b94a7b921f4
Bojan Skrchevski <bojanskr@gmail.com>
2018-03-22 15:31:12 +0100

    Implemented finding prime numbers algorithm

---
+++ algorithms/primes/primes.cs
@@ -0,0 +1,47 @@
+  /*
+  *
+  * Bojan Skrchevski 2018-03-22
+  *
+  * Prime numbers algorithm
+  * 
+  * Originaly posted on HackerRank: https://www.hackerrank.com/challenges/ctci-big-o/problem
+  *
+  *
+  */
+
+  static void Main(String[] args) 
+  {
+        int p = Convert.ToInt32(Console.ReadLine());
+        for(int a0 = 0; a0 < p; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Out.WriteLine(isPrime(n) ? "Prime" : "Not prime");
+        }
+    }
+    
+    static bool isPrime(int n)
+    {
+        if (n < 2) 
+        {
+            return false;
+        } 
+        else if (n == 2) 
+        {
+            return true;
+        } 
+        // skip all divisible by 2 and 3 (no need, since they are not primes)
+        else if (n % 2 == 0 || n % 3 == 0) 
+        {
+            return false;
+        }
+        
+        // go only to sqrt(n)
+        var sqrtN = Math.Sqrt(n);
+        for (int i=3; i<(int)sqrtN+1; i++)
+        {
+            if(n % i == 0) return false;
+        }
+        
+        return true;
+    }
