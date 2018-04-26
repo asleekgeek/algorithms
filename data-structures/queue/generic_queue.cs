bc4562a26ee69bbfeae6f730106519850431407c
Bojan Skrchevski <bojanskr@gmail.com>
2018-04-26 16:08:47 +0200

    Code formatting fix for generic Queue in c#

--- data-structures/queue/generic_queue.cs
+++ data-structures/queue/generic_queue.cs
@@ -9,51 +9,46 @@ public class Queue<T>
     private Node<T> head = null;
     private Node<T> tail = null;
 
-    public void Enqueue(T data)
-    {
-        var node = new Node<T>
-        {
+    public void Enqueue(T data) {
+        
+        var node = new Node<T> {
             data = data,
             next = null
         };
         
-        if(tail != null)
-        {
+        if(tail != null) {
             tail.next = node;
         }
 
         tail = node;
 
-        if(head == null)
-        {
+        if(head == null) {
             head = node;
         }
     }
 
-    public T Dequeue()
-    {
+    public T Dequeue() {
+    
         if (IsEmpty()) throw new QueueEmptyException();
 
         var firstValue = head.data;
         head = head.next;
 
-        if(IsEmpty())
-        {
+        if(IsEmpty()) {
             tail = null;
         }
 
         return firstValue;
     }
 
-    public T Peek()
-    {
+    public T Peek() {
+    
         if (IsEmpty()) throw new QueueEmptyException();
 
         return head.data;
     }
 
-    public bool IsEmpty()
-    {
+    public bool IsEmpty() {
         return head == null;
     }
 }
